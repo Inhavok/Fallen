21d01fcc8e702741b4ceeae1f9a4e60d49c956b6
Pizazzle <zebehrman01@gmail.com>
2015-10-10 16:32:18 -0400

    Layer Update

--- core/src/com/inhavok/fallen/components/entity_components/Layer.java
+++ core/src/com/inhavok/fallen/components/entity_components/Layer.java
@@ -1,7 +1,10 @@
 package com.inhavok.fallen.components.entity_components;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.LinkedList;
 
+import com.badlogic.gdx.graphics.g2d.Animation;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.physics.box2d.Fixture;
 
@@ -9,22 +12,27 @@ class Layer {
 	
 	//O
 	
+	private HashMap<String, Animation> animations = new HashMap<String, Animation>();
+	private Fixture f;
+	private float stateTime;
 	
-	public Layer(int layers) {
+	
+	public Layer(HashMap<String, Animation> animations, Fixture f) {
+		this.f = f;
+		this.animations = animations;
+	}
+	
+	public Layer (HashMap<String, Animation> animations) {
+		this(animations, null);
+	}
+	
+	public Sprite getSprite() {
+		Sprite sprite;
+		return sprite.setRegion((new ArrayList<Animation>(animations)).get(0).getKeyFrame(stateTime));
 		
-		for(int x = 1; x <= layers; x++) {
-			
-		}
 		
 	}
 	
-	public void addSprites(Fixture f, Sprite... s) {
-		
-	}
-	
-	public void addAnimation(Fixture f, Sprite[] anim) {
-		
-	}
 	
 	
 }
