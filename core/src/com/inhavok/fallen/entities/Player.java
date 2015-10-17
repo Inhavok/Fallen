f1d2db8591be282d6a422cdd4da73463e5a53d9b
Pizazzle56 <meowcatcompany@gmail.com>
2015-10-16 17:17:53 -0700

    Fix #1 to player animation

--- core/src/com/inhavok/fallen/entities/Player.java
+++ core/src/com/inhavok/fallen/entities/Player.java
@@ -32,29 +32,33 @@ public final class Player extends Entity {
 	}
 	public void faceCursor() {
 		execute(new GraphicsSetLayerRotation(PlayerGraphics.Layer.TORSO, MathUtils.atan2((Gdx.graphics.getHeight() - Gdx.input.getY() - Gdx.graphics.getHeight() / 2) / (float) Application.PIXELS_PER_METER, (Gdx.input.getX() - Gdx.graphics.getWidth() / 2) / (float) Application.PIXELS_PER_METER) * MathUtils.radiansToDegrees - 90));
+		execute(new GraphicsSetLayerRotation(PlayerGraphics.Layer.LEGS, MathUtils.atan2((Gdx.graphics.getHeight() - Gdx.input.getY() - Gdx.graphics.getHeight() / 2) / (float) Application.PIXELS_PER_METER, (Gdx.input.getX() - Gdx.graphics.getWidth() / 2) / (float) Application.PIXELS_PER_METER) * MathUtils.radiansToDegrees - 90));
 	}
-	public void walk(final Direction direction) {
+	public void walk(Direction direction) {
 		int impulse = 3;
 		switch (direction) {
 			case UP:
 				execute(new PhysicsApplyLinearImpulse(0, impulse));
-				execute(new GraphicsSetLayerRotation(PlayerGraphics.Layer.LEGS, 0));
+				//execute(new GraphicsSetLayerRotation(PlayerGraphics.Layer.LEGS, 0));
 				break;
 			case DOWN:
 				execute(new PhysicsApplyLinearImpulse(0, -impulse));
-				execute(new GraphicsSetLayerRotation(PlayerGraphics.Layer.LEGS, 180));
+				//execute(new GraphicsSetLayerRotation(PlayerGraphics.Layer.LEGS, 180));
 				break;
 			case LEFT:
 				execute(new PhysicsApplyLinearImpulse(-impulse, 0));
-				execute(new GraphicsSetLayerRotation(PlayerGraphics.Layer.LEGS, 90));
+				//execute(new GraphicsSetLayerRotation(PlayerGraphics.Layer.LEGS, 90));
 				break;
 			case RIGHT:
 				execute(new PhysicsApplyLinearImpulse(impulse, 0));
-				execute(new GraphicsSetLayerRotation(PlayerGraphics.Layer.LEGS, 270));
+				//execute(new GraphicsSetLayerRotation(PlayerGraphics.Layer.LEGS, 270));
 				break;
+
 		}
 		execute(new GraphicsSetAnimation(PlayerGraphics.Layer.LEGS, PlayerLegsLayer.Animation.WALKING));
 	}
+
+
 	public void stopWalking() {
 		execute(new GraphicsSetAnimation(PlayerGraphics.Layer.LEGS, PlayerLegsLayer.Animation.IDLE));
 	}
