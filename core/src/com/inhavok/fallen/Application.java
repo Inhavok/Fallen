3958ed64804ddeed01459d6c70c1b35daadeecea
Ben Cripps <ocordu@gmail.com>
2016-01-07 14:15:42 +0000

    Reverted to LWJGL 2 and OpenGL 2.

--- core/src/com/inhavok/fallen/Application.java
+++ core/src/com/inhavok/fallen/Application.java
@@ -2,7 +2,7 @@ package com.inhavok.fallen;
 
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
-import com.badlogic.gdx.graphics.GL30;
+import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
 import com.inhavok.fallen.commands.Command;
@@ -58,7 +58,7 @@ public final class Application extends ApplicationAdapter {
 	}
 	private static void drawGraphics() {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
-		Gdx.gl.glClear(GL30.GL_COLOR_BUFFER_BIT);
+		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 		currentState.execute(new EntitiesDraw(spriteBatch));
 		StateUI.draw();
 	}
