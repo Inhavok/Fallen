f128bc95802537deca4d166b23a031cbc9616a9a
Ben <ocordu@gmail.com>
2016-04-16 21:37:18 +0100

    Cleaned up imports.

--- core/src/com/inhavok/fallen/entity_components/controllers/PlayerController.java
+++ core/src/com/inhavok/fallen/entity_components/controllers/PlayerController.java
@@ -2,7 +2,6 @@ package com.inhavok.fallen.entity_components.controllers;
 
 import com.badlogic.gdx.Input;
 import com.badlogic.gdx.math.Vector2;
-import com.inhavok.fallen.entity_components.controllers.EntityController;
 import com.inhavok.fallen.state_components.StateUI;
 import com.inhavok.fallen.entities.characters.Player;
 
